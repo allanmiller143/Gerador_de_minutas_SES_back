581ad58d7a537590d159bcad11caa665280c5a48
Pedro Afonso <salvadorpedroafonso@gmail.com>
2026-06-05 11:15:28 -0300

    feat: DASH-04

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from datetime import datetime
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
@@ -48,3 +49,23 @@ class Role(db.Model):
 
     def __repr__(self):
         return f'<Role {self.name}>'
+
+
+class ProcessoSEI(db.Model):
+    __tablename__ = 'processos_sei'
+    
+    id = db.Column(db.Integer, primary_key=True) 
+    numero = db.Column(db.String(50), unique=True, nullable=False)
+    assunto = db.Column(db.String(200), nullable=False)
+    status = db.Column(db.String(50), nullable=False) 
+    dataRecebimento = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    prioridade = db.Column(db.String(50), nullable=False)
+    iaConfidence = db.Column(db.Float, nullable=False, default=0.0)
+    analista = db.Column(db.String(100), nullable=True) 
+    dataRevisao = db.Column(db.DateTime, nullable=True) 
+    dataPreAnalise = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    iaSugestao = db.Column(db.Text, nullable=True)
+    jurisprudenciasSugeridas = db.Column(db.JSON, nullable=False, default=list)
+
+    def __repr__(self):
+        return f'<ProcessoSEI {self.numero}>'
