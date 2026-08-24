1e6e970e1a5a8db9abf4e47b7379696e0721665a
Emanoel Barreiros <emanoelbarreiros@gmail.com>
2026-08-24 17:43:32 -0300

    chore: merge migration heads and add existence checks to erro_processamento column operations

---
+++ migrations/versions/0d0a33a45e21_merge_heads.py
@@ -0,0 +1,24 @@
+"""merge heads
+
+Revision ID: 0d0a33a45e21
+Revises: 5f8e9d2c10ab, 6044237836d7
+Create Date: 2026-08-12 10:29:20.969793
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '0d0a33a45e21'
+down_revision = ('5f8e9d2c10ab', '6044237836d7')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
